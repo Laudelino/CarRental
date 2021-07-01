8d749042f10b79347a1cda4c2ceec42109ac9108
Laudelino Adao <lalau_aj@hotmail.com>
2021-07-01 15:40:53 -0300

    Initial commit

---
+++ CarRental.API.Vehicles.Tests/VehiclesServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CarRental.API.Vehicles.Tests
+{
+    public class VehiclesServiceTest
+    {
+        [Fact]
+        public void GetVehiclesReturnsAllVehicles()
+        {
+
+        }
+    }
+}
